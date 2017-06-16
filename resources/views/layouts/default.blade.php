d7fce2e8a05e923594605fb975245d8c97d137a4
JaySong <56189502@qq.com>
2017-06-16 05:27:32 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>@yield('title','Sample App') - laravel 入门教程</title>
-  <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
-  @include('layouts._header')
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
 
-  <div class="container">
-    @include('shared.messages')
-    @yield('content')
-    @include('layouts._footer')
-  </div>
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @include('shared.messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
 
-</body>
+    <script src="/js/app.js"></script>
+  </body>
 </html>
